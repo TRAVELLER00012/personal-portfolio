8ab154f3712f9f33ae0fc6586d9f37c914404313
Daksh Kumar <k.daksh29@gmail.com>
2025-05-07 10:24:18 +0530

    add contact me with email in mobile version of navbar

--- src/app/Navbar/main.tsx
+++ src/app/Navbar/main.tsx
@@ -24,6 +24,7 @@ const Navbar = () => {
                   <li><a href="#home">Home</a></li>
                   <li><a href="#about">About</a></li>
                   <li><a href="#skills">Skills</a></li>
+                  <li><a href="mailto:k.daksh29@gmail.com">Contact Me!</a></li>
               </ul>
         </div>
       </div>
